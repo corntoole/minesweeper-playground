0e8abfc7d1ace35f9fea88cad26793cbe6aacb37
Cornelius Toole <cornelius.toole@gmail.com>
2016-05-11 08:36:11 -0500

    Minesweeper game
      - most of the game is implemented
      - TODO: implement logic for a won game
      - TODO: implement interactive gameplay loop
      - TODO: implement a solver

--- minesweeper.py
+++ minesweeper.py
@@ -42,20 +42,66 @@ class Minesweeper(object):
         self.remaining_flags = number_of_mines
         self.compute_mine_adjacency()
 
+    def _render_cell(self, cell):
+        if self._state is MinesweeperStates.new_game:
+            return '_'
+        elif self._state is MinesweeperStates.in_progress:
+            if cell['state'] == 'covered':
+                return '_'
+            elif cell['state'] == 'uncovered':
+                if cell['adjacent_mines'] > 0:
+                    return str(cell['adjacent_mines']) # show number of adjacent mines
+                else:
+                    return 'X'
+            else: # then cell['state'] == 'marked'
+                return 'M'
+        elif self._state is MinesweeperStates.game_lost:
+            if cell['state'] == 'marked':
+                if cell['is_armed'] is False:
+                    return '%'
+                else:
+                    return 'M'
+            elif cell['state'] == 'uncovered' and cell['is_armed']:
+                return '@'
+            elif cell['is_armed'] and cell['state'] == 'covered':
+                return '*'
+            else:
+                if cell['adjacent_mines'] > 0:
+                    return str(cell['adjacent_mines']) # show number of adjacent mines
+                else:
+                    return 'X'
+                return 'u'
+    def is_game_over(self):
+        return self._state == MinesweeperStates.game_lost
+
     def show(self):
         print ("  " + "".join([' {} '.format(i) for i in range(len(self.board))]))
 
         for row, row_number in zip(self.board, range(len(self.board))):
-            print("{} ".format(row_number) + "".join([' {} '.format(Minesweeper.grid_repr[cell['state']]) for cell in row]))
+            print("{} ".format(row_number) + "".join([' {} '.format(self._render_cell(cell)) for cell in row]))
+
+        print (self._state)
 
         # for row in self.board:
         #     print([item['adjacent_mines'] for item in row])
 
         # print(self.mines)
-
+    # def update_state(self, operation):
+    #     if self._state is MinesweeperStates.new_game:
+    #         if operation is 'mark':
+    #             self._state = MinesweeperStates.in_progress
+    #
+    #     elif self._state is MinesweeperStates.in_progress:
+    #         pass
+    #     elif self._state is MinesweeperStates.game_lost:
+    #         pass
+    #     else:
+    #         pass
     def mark_cell(self, x, y):
         # update cell.state
         self.board[y][x]['state'] = 'marked'
+        if self._state is MinesweeperStates.new_game:
+            self._state = MinesweeperStates.in_progress
         # update remaining flags
         self.remaining_flags = self.remaining_flags - 1
         self.flags.append((y,x))
@@ -64,6 +110,7 @@ class Minesweeper(object):
         if self.board[y][x]['is_armed'] == False:
             self.board[y][x]['state'] = 'uncovered'
             self.uncover_empty_neighbors(y,x)
+            self._state = MinesweeperStates.in_progress
         else:
             self.board[x][y]['state'] = 'uncovered'
             self._state = MinesweeperStates.game_lost
@@ -86,3 +133,39 @@ class MinesweeperGenerationalUncover(Minesweeper):
             if self.board[r][c]['state'] == 'empty':
                 self.board[r][c]['state'] = 'uncovered'
                 self.uncover_empty_neighbors(r,c, depth-1)
+
+def test1(dim = 7, num_mines = 10):
+    preamble ='''-------------------------------------------------------------------------------
+|   New Game                                                                   |
+-------------------------------------------------------------------------------
+'''
+    print(preamble)
+    mine = Minesweeper(dim, num_mines)
+    for i in range(dim*dim):
+        x,y = (random.randint(0,dim-1),random.randint(0,dim-1))
+        print ("Move #{} at cell({},{})".format(i+1,x,y))
+        mine.uncover_cell(y,x)
+        mine.show()
+        if mine.is_game_over():
+            return
+
+
+def test2(dim = 7, num_mines = 10):
+    preamble ='''-------------------------------------------------------------------------------
+|   New Game                                                                   |
+-------------------------------------------------------------------------------
+'''
+    print(preamble)
+    mine = Minesweeper(dim, num_mines)
+    for j in range(num_mines):
+        x,y = (random.randint(0,dim-1),random.randint(0,dim-1))
+        print ("Mark #{} at cell({},{})".format(j+1,x,y))
+        mine.mark_cell(y,x)
+        mine.show()
+    for i in range(dim*dim):
+        x,y = (random.randint(0,dim-1),random.randint(0,dim-1))
+        print ("Move #{} at cell({},{})".format(i+1,x,y))
+        mine.uncover_cell(y,x)
+        mine.show()
+        if mine.is_game_over():
+            return
